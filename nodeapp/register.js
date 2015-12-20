abc17febf21fcb2d4cd0084b31eb929e499d0201
Chris Neave <chrispneave@gmail.com>
2015-12-20 00:03:28 +0000

    nodeapp registers with Consul using the Agent API

---
+++ nodeapp/register.js
@@ -0,0 +1,22 @@
+var uuid = require('uuid');
+var consul = require('consul');
+var appUuid = uuid.v4();
+
+module.exports = function(options, callback) {
+  var client = consul({
+    host: process.env['CONSUL_HOST'],
+    port: process.env['CONSUL_PORT']
+  });
+
+  client.agent.service.register({
+    name: options.app,
+    id: options.app + '_' + appUuid,
+    tags: options.tags,
+    address: options.address,
+    port: options.port,
+    check: options.check
+  }, function(err) {
+    if (err) { return callback(err); }
+    callback('ok');
+  });
+}
--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,9 +1,14 @@
+var url = require('url');
+var ip = require('ip');
 var express = require('express');
 var morgan = require('morgan');
 var onFinished = require('on-finished');
 var Prometheus = require('prometheus-client');
 var metrics = new Prometheus();
 var nodeMetrics = require('./nodeMetrics');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({ name: 'nodeapp' });
+var registerWithConsul = require('./register.js');
 var metricsApp = express();
 var app = express();
 
@@ -43,11 +48,30 @@ function createRequestCounter(endpointName) {
 }
 
 metricsApp.use(morgan('dev'));
+metricsApp.get('/', function(req, res, next) {
+  res.status(200).send('Hello, World!').end();
+  next();
+});
 metricsApp.get('/metrics', metrics.metricsFunc());
 
-metricsApp.listen(9090, function() {
-  console.log('Metrics API listening on port 9090');
+registerWithConsul({
+  app: 'nodeapp_metrics',
+  port: 9090,
+  tags: ['metrics'],
+  address: ip.address(),
+  check: {
+    http: url.format({ protocol: 'http', hostname: ip.address(), port: 9090 }),
+    interval: '5s',
+    timeout: '1s'
+  }
+}, function(result) {
+  log.info('Registered with Consul => ' + result);
+
+  metricsApp.listen(9090, function() {
+    log.info('Metrics API listening on port 9090');
+  });
 });
+
 
 app.use(morgan('dev'));
 app.use(createRequestCounter('nodeapp'));
@@ -62,6 +86,20 @@ app.get('/slow', function(req, res, next) {
   }, Math.random() * 1000);
 });
 
-var server = app.listen(3000, function() {
-  console.log('nodeapp listening on port 3000');
+registerWithConsul({
+  app: 'nodeapp',
+  port: 3000,
+  tags: ['app'],
+  address: ip.address(),
+  check: {
+    http: url.format({ protocol: 'http', hostname: ip.address(), port: 3000 }),
+    interval: '5s',
+    timeout: '1s'
+  }
+}, function(result) {
+  log.info('Registered with Consul => ' + result);
+
+  var server = app.listen(3000, function() {
+    log.info('nodeapp listening on port 3000');
+  });
 });
